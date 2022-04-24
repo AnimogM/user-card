881752405dfe7d0e8e3ba5e36797197e9d46b5c6
Gomina Maryam <gominamaryam@gmail.com>
2022-04-24 03:24:56 +0100

    users

--- src/App.js
+++ src/App.js
@@ -1,185 +1,30 @@
-import { useState } from "react";
-import {
-  Button,
-  Form,
-  Container,
-  Navbar,
-  NavbarBrand,
-  Row,
-  Col,
-} from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Container, Navbar, NavbarBrand, Row } from "react-bootstrap";
+
+import Card from "./Card";
 
 function App() {
-  const [show, setShow] = useState(false);
-  const [values, setValues] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    address: "",
-    gender: "",
-    bio: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues((preValues) => {
-      return { ...preValues, [name]: value };
-    });
-    setShow(false);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShow(true);
-  };
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
   return (
     <div>
-      <Navbar bg="primary">
+      <Navbar bg="success">
         <Container>
-          <NavbarBrand className="text-white fs-4">React Form</NavbarBrand>
+          <NavbarBrand className="text-white fs-4">Users</NavbarBrand>
         </Container>
       </Navbar>
       <div className="bg-light py-5">
         <Container>
-          {show && (
-            <div className="d-flex flex-column align-items-center ">
-              <h3>Your Details</h3>
-              <div>
-                <span className="fw-bold">First Name</span>: {values.firstName}
-              </div>
-              <div>
-                <span className="fw-bold">Last Name</span>: {values.lastName}
-              </div>
-              <div>
-                <span className="fw-bold">Email</span>: {values.email}
-              </div>
-              <div>
-                <span className="fw-bold">Address</span>: {values.address}
-              </div>
-              <div>
-                <span className="fw-bold">Gender</span>: {values.gender}
-              </div>
-              <div>
-                <span className="fw-bold">Bio</span>: {values.bio}
-              </div>
-            </div>
-          )}
-          <Form onSubmit={handleSubmit}>
-            {show || <h3 className="text-center">Enter Your Details</h3>}
-            <Row className="mt-4 justify-content-center gap-3">
-              <Col md={7}>
-                <input
-                  name="firstName"
-                  id="firstName"
-                  className="py-2 form-control"
-                  required
-                  type="text"
-                  value={values.firstName}
-                  onChange={handleChange}
-                  placeholder="First Name"
-                />
-              </Col>
-              <Col md={7}>
-                <input
-                  name="lastName"
-                  id="lastName"
-                  className="py-2 form-control"
-                  required
-                  type="text"
-                  value={values.lastName}
-                  onChange={handleChange}
-                  placeholder="Last Name"
-                />
-              </Col>
-              <Col md={7}>
-                <input
-                  name="email"
-                  id="email"
-                  className="py-2 form-control"
-                  required
-                  type="email"
-                  value={values.email}
-                  onChange={handleChange}
-                  placeholder="Email"
-                />
-              </Col>
-              <Col md={7}>
-                <input
-                  name="address"
-                  id="address"
-                  className="py-2 form-control"
-                  required
-                  type="text"
-                  value={values.address}
-                  onChange={handleChange}
-                  placeholder="Address"
-                />
-              </Col>
-              <Col md={7}>
-                <legend className="fs-5">Gender</legend>
-                <div className="d-flex gap-4">
-                  <div>
-                    <label className="ms-2" htmlFor="male">
-                      <input
-                        name="gender"
-                        id="male"
-                        value="male"
-                        required
-                        type="radio"
-                        checked={values.gender === "male"}
-                        onChange={handleChange}
-                      />{" "}
-                      Male
-                    </label>
-                  </div>
-                  <div>
-                    <label className="ms-2" htmlFor="female">
-                      <input
-                        name="gender"
-                        id="female"
-                        value="female"
-                        required
-                        type="radio"
-                        checked={values.gender === "female"}
-                        onChange={handleChange}
-                      />{" "}
-                      Female
-                    </label>
-                  </div>
-                  <div>
-                    <label className="ms-2" htmlFor="others">
-                      <input
-                        name="gender"
-                        id="others"
-                        required
-                        type="radio"
-                        value="others"
-                        checked={values.gender === "others"}
-                        onChange={handleChange}
-                      />{" "}
-                      Others
-                    </label>
-                  </div>
-                </div>
-              </Col>
-              <Col md={7}>
-                <textarea
-                  name="bio"
-                  id="bio"
-                  required
-                  className="py-2 form-control"
-                  placeholder="Your Bio"
-                  value={values.bio}
-                  onChange={handleChange}
-                ></textarea>
-              </Col>
-              <Col md={7}>
-                <Button type="submit" className="form-control" size="lg">
-                  Submit
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+          <Row className="gap-5 justify-content-around px-3">
+            {users.map((user) => (
+              <Card key={user.id} {...user} />
+            ))}
+          </Row>
         </Container>
       </div>
       <footer className="bg-black text-center py-3 text-white">
---
+++ src/Card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Col } from 'react-bootstrap'
+
+const Card = ({
+  name,
+  company: { catchPhrase },
+  phone,
+    email,
+  username,
+  address: { street, suite, city},
+}) => {
+  return (
+    <Col className="bg-card px-3 px-md-5 py-2 shadow" lg={5}>
+      <h1 className="display-3 fw-bold">{name}</h1>
+      <p>{catchPhrase}</p>
+      <div className="pt-3">
+        <h3 className="pb-md-3">General Info </h3>
+        <p className=" mb-2 row">
+          <span className=" col-md-3 fw-bold">Username</span>{" "}
+          <span className=" col-md-9 fw-normal">{username}</span>
+        </p>
+        <p className=" mb-2 row">
+          <span className=" col-md-3 fw-bold">Address</span>
+          <span className=" col-md-9 fw-normal">
+            {street}, {suite}, {city}
+          </span>
+        </p>
+        <p className="row mb-2">
+          <span className=" col-md-3 fw-bold">Email</span>
+          <span className=" col-md-9">{email}</span>
+        </p>
+        <p className="row mb-2">
+          <span className=" col-md-3 fw-bold">Phone</span>
+          <span className=" col-md-9">{phone}</span>
+        </p>
+      </div>
+    </Col>
+  );
+};
+
+export default Card
